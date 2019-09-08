2bb771481e181cbb691b2d5cf025205317dd4ee7
AndrewLam97 <alam1997@gmail.com>
2019-09-08 01:42:45 -0700

    Add files via upload
    Streamlined variable initialization and updated comments

--- CIS014_Hw1/CIS014_Hw1.cpp
+++ CIS014_Hw1/CIS014_Hw1.cpp
@@ -1,23 +1,30 @@
-// CIS014_Hw1.cpp : Andrew Lam
+// CIS014_Hw1.cpp : Andrew Lam 9-2-19
 // This is a console application that takes a user's numeric "pennies" input and returns a string output (using the USD units of currency).
 // The program should then convert USD to CAD.
 
 #include <iostream>
 using namespace std;
+#include <math.h>
 
 int main()
 {
-	int pennies;
+	const float convRate = 1.33;
+
+	// Initializes required variables for coin types
+	int pennies = 0, 
+		dollars = 0,
+		quarters = 0,
+		dimes = 0,
+		nickels = 0;
+
+	// Takes and stores user input for total number of pennies
 	cout << "Please enter all your pennies: ";
 	cin >> pennies;
 
-	int dollars;
-	int quarters;
-	int dimes;
-	int nickels;
+	// Converts US pennies to CAD
+	float cad = (float(pennies) / 100) * convRate;
 
-	float cad = (float(pennies) / 100) * 1.33;
-
+	// Reduces pennies to the two largest denominations available and subtracts from total penny count until pennies are less than the value of a nickel
 	while (pennies > 5) {
 		dollars = floor(pennies / 100);
 		quarters = floor((pennies % 100) / 25);
@@ -27,11 +34,12 @@ int main()
 		pennies -= (dimes * 10) + (nickels * 5);
 	}
 
-	cout << "In US currency you have " << dollars << " dollars, " << quarters << " quarters, " << dimes << " dimes, " << nickels << " nickels, and " << pennies << " pennies. \n";
-
+	// Prints desired monetary conversions
+	cout << "In US currency you have: " << dollars << " dollars, " << quarters << " quarters, " << dimes << " dimes, " << nickels << " nickels, and " << pennies << " pennies. \n";
+		
 	cout << "In Canadian currency you have: " << cad << " dollars. \n";
 
 	system("PAUSE");
-	
+
 	return 0;
 }
