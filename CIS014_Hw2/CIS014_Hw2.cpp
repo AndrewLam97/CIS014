c3febe428ac5c38028de5a69a38103737cc480b4
AndrewLam97 <alam1997@gmail.com>
2019-09-15 12:23:19 -0700

    Update CIS014_Hw2.cpp

--- CIS014_Hw2/CIS014_Hw2.cpp
+++ CIS014_Hw2/CIS014_Hw2.cpp
@@ -1,6 +1,8 @@
 	/**
- * CIS014_Hw2.1	Andrew Lam 9-7-19
-
+ * CIS014_Hw2.1
+ * Andrew Lam
+ * 9-7-19
+ *
  * FUNCTION NAME: breakCash
  * PURPOSE: the function takes in the total of pennies from the user as input, computes its equivalent in
  * dollars, quarters, dimes, nickels, and pennies, and outputs the result in string to the console
@@ -18,15 +20,15 @@ using namespace std;
 #include <math.h>
 
 
-string breakCash(int numPennies) {
+string breakCash(int numPennies) { //breakCash function takes number of pennies and returns a string
 	int numDollars = 0,
 		numQuarters = 0,
 		numDimes = 0,
 		numNickels = 0;
 
-	float numCad = numPennies * 0.0133;
+	float numCad = numPennies * 0.0133; //USD to CAD conversion
 
-	while (numPennies > 5) {
+	while (numPennies > 5) { //calculates maximum largest denomination and subtracts from numPennies
 		numDollars = floor(numPennies / 100);
 		numQuarters = floor((numPennies % 100) / 25);
 		numPennies -= (numDollars * 100) + (numQuarters * 25);
@@ -35,8 +37,8 @@ string breakCash(int numPennies) {
 		numPennies -= (numDimes * 10) + (numNickels * 5);
 	}
 
-	char result[150];
-	sprintf_s(result, "In US currency you have: %d dollar(s), %d quarter(s), %d dime(s), %d nickel(s), and %d pennies. \nIn Canadian currency you have %.2f dollars. \n", numDollars, numQuarters, numDimes, numNickels, numPennies, numCad);
+	char result[150]; //constructs string buffer for return
+	sprintf(result, "In US currency you have: %d dollars, %d quarters, %d dimes, %d nickels, and %d pennies. \nIn Canadian currency you have %.2f dollars. \n", numDollars, numQuarters, numDimes, numNickels, numPennies, numCad);
 
 	return result;
 }
@@ -44,7 +46,8 @@ string breakCash(int numPennies) {
 int main()
 {
 	int numPennies = 0;
-	cout << "Please enter all your pennies: ";
+	cout << "Please enter all of your pennies: ";
 	cin >> numPennies;
 	cout << breakCash(numPennies);
+	return 0;
 }
