73d471427c27aef65b11c36a57d9a00fe369ee68
AndrewLam97 <alam1997@gmail.com>
2019-09-17 19:52:00 -0700

    add CIS014_Hw3.cpp

--- CIS014_Hw3/CIS014_Hw3/CIS014_Hw3.cpp
+++ CIS014_Hw3/CIS014_Hw3/CIS014_Hw3.cpp
@@ -1,16 +1,26 @@
 /*
-CIS014 HW3
-Andrew Lam
-9-16-19
-*/
+ *	CIS014 HW3
+ *	Andrew Lam
+ *	DUE: 9-22-19
+ *	PURPOSE: Separate all of the 'a' and 'b' in a string to that all the 'a' appear before the 'b' in it.
+ */
+
 
 #include <iostream>
 using namespace std;
 #include <string>
+#include <algorithm>
 
+/*
+ *  NAME: separateLetters
+ *	PURPOSE: This function separates all 'a' and 'b' in a string so that all 'a' appear before 'b'
+ *	PARAMETERS: string containing 'a' and 'b'
+ *	RETURN VALUES: sorted string containing 'a' and 'b'
+*/
 string separateLetters(string input)
 {
 	int aCounter = 0;
+	string result = "";
 
 	for (int i = 0; i < input.length(); i++)
 	{
@@ -19,8 +29,6 @@ string separateLetters(string input)
 			aCounter++;
 		}
 	}
-	cout << endl << aCounter;
-	string result = "";
 
 	for (int j = 0; j < aCounter; j++)
 	{
@@ -37,6 +45,7 @@ string separateLetters(string input)
 
 int main()
 {
-	cout << "aaabababbba" << endl;
-	cout << separateLetters("aaabababbba");
+	string testString = "aaababbaba"; //6a4b
+//	cout << testString << endl;
+	cout << bool(separateLetters(testString) == "aaaaaabbbb");
 }
