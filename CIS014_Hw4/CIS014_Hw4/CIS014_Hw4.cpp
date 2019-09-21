43ea237d97b6cb3e96a2292cf3e0b263c745f9c7
AndrewLam97 <alam1997@gmail.com>
2019-09-21 14:10:22 -0700

    add CIS014_Hw4

---
+++ CIS014_Hw4/CIS014_Hw4/CIS014_Hw4.cpp
@@ -0,0 +1,17 @@
+// CIS014_Hw4.cpp
+// Check if initial input is palindrome
+// Check from right to left for largest sub-palindrome
+// Find pivot point, check +/- around pivot
+// Find # of digits before largest sub palindrome * 2 + sub-palindrome length
+// If none, shortest possible is 2x-1
+// 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
