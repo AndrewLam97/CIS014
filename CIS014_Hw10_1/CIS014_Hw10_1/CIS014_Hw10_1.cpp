81f2f7beacd92810678af4d54761c1309886d0c5
AndrewLam97 <alam1997@gmail.com>
2019-11-15 07:45:06 -0800

    Update CIS014_Hw10_1.cpp

--- CIS014_Hw10_1/CIS014_Hw10_1/CIS014_Hw10_1.cpp
+++ CIS014_Hw10_1/CIS014_Hw10_1/CIS014_Hw10_1.cpp
@@ -1,30 +1,24 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
 
 class CIS14 {
 public:
 	int getMaxPoints(vector<vector<int>>& maze);
-private:
-	vector<int> results;
 };
 
 int CIS14::getMaxPoints(vector<vector<int>>& maze)
 {
-	int subtotal = 0;
-	if (maze.size() == 0) return 0;
-	
-	for (int i = 0; i < maze.size(); i++)
-	{
-		for (int j = 0; j < maze[i].size(); j++)
-		{
-
-		}
-	}
+	for (int i = maze.size() - 2; i >= 0; i--)
+		for (int j = 0; j <= i; j++)
+			maze[i + 1][j] > maze[i + 1][j + 1] ? maze[i][j] += maze[i + 1][j] : maze[i][j] += maze[i + 1][j + 1];
+	return maze[0][0];
 }
-
 
 int main()
 {
-
+	CIS14 cis14;
+	vector<vector<int>> maze = { {1}, {1,22305}, {1,29394,1}, {1,1,1234,1}, {1,1,3,1,10} };
+	cout << cis14.getMaxPoints(maze);
 }
