9595a7adb8033000ede701b35187a5bd83cb5441
AndrewLam97 <alam1997@gmail.com>
2019-11-18 02:39:54 -0800

    Update CIS014_Hw10_1 and CIS014_Hw10_2

--- CIS014_Hw10_1/CIS014_Hw10_1/CIS014_Hw10_1.cpp
+++ CIS014_Hw10_1/CIS014_Hw10_1/CIS014_Hw10_1.cpp
@@ -1,3 +1,8 @@
+/*
+ASSIGNMENT: 10.1
+DUE: 11/17/19
+PURPOSE: Produce the max amount of points possible given a pyramid maze for pinball
+*/
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -8,6 +13,11 @@ public:
 	int getMaxPoints(vector<vector<int>>& maze);
 };
 
+/*
+PURPOSE: Traverse maze from top-down in a pinball-like fashion and find max amount of points 
+PARAMETERS: vector of integer vectors representing maze
+RETURN VALUES: intger of max points possible
+*/
 int CIS14::getMaxPoints(vector<vector<int>>& maze)
 {
 	if (maze.size() == 0 || maze[0].size() == 0) //handles empty vector array and an array of empty vectors
@@ -25,6 +35,19 @@ int CIS14::getMaxPoints(vector<vector<int>>& maze)
 int main()
 {
 	CIS14 cis14;
-	vector<vector<int>> maze = { {0}, {7,4}, {2,4,6}, {8,5,9,3} };
+	vector<vector<int>> maze = { {1}, {1,-10}, {1,30,1}, {1,-1,-1,1} };
 	cout << cis14.getMaxPoints(maze);
 }
+
+/*
+TEST CASES:
+{{0}, {4,1}, {5,3,8}, {1,6,7,3}};	16
+
+vector<vector<int>> maze = { {1}, {1,1}, {1,1,1}, {1,1,1,1} };		4
+
+vector<vector<int>> maze = { {3}, {7,4}, {2,4,6}, {8,5,9,3} };		23
+
+vector<vector<int>> maze = { {1}, {1,22305}, {1,29394,1}, {1,1,1234,1}, {1,1,3,1,10} };		52937
+
+vector<vector<int>> maze = { {1}, {1,-10}, {1,30,1}, {1,-1,-1,1} };		31
+*/
--- CIS014_Hw10_2/CIS014_Hw10_2/CIS014_Hw10_2.cpp
+++ CIS014_Hw10_2/CIS014_Hw10_2/CIS014_Hw10_2.cpp
@@ -1,3 +1,8 @@
+/*
+ASSIGNMENT: 10.2
+DUE: 11/17/19
+PURPOSE: Outputs the closest common waypoint from any given two path input, Node* A, and Node* B
+*/
 #include <iostream>
 using namespace std;
 
@@ -13,29 +18,71 @@ public:
 	Node* getClosestCommonWaypoint(Node* A, Node* B);
 };
 
+/*
+PURPOSE: Outputs the closest common waypoint from any given two path input, Node* A, and Node* B
+PARAMETERS: Node pointer A and B containing a path in the form a linked list
+RETURN VALUES: Node pointer of closest common waypoint
+*/
 Node* CIS14::getClosestCommonWaypoint(Node* A, Node* B)
 {
 	Node* tmp = A;
 	Node* tmp1 = B;
-	while (tmp->next != nullptr)
-	{
-		while (tmp1->next != nullptr)
-		{
 
-			tmp1 = tmp1->next;
-		}
-		tmp = tmp->next;
+	if (A == nullptr || B == nullptr) //check if inputs are nullptrs
+	{
+		return nullptr;
 	}
+
+	while (tmp != nullptr) //while not at end of path
+	{
+		while (tmp1 != nullptr) //while not at end of other path
+		{
+			if (tmp == tmp1) //check equivalence
+			{
+				return tmp;
+			}
+			tmp1 = tmp1->next; //iterate pointer by setting current pointer to next
+		}
+		tmp1 = B; //reset inner loop
+		tmp = tmp->next; //iterate outer loop's pointer
+	}
+	return nullptr;
 }
 
 int main()
 {
 	CIS14 cis14;
 
-	Node a(10), a1(11), a2(12), a3(13), a4(14), b1(21), b2(22), c1(31), c2(32);
-	a.next = &a1; a1.next = &a2; a2.next = &a3; a3.next = &a4; a4.next = &c1;
+	Node  a1(11), a2(12), a3(13), b1(21), b2(22), c1(31), c2(32), d1(99);
+	a1.next = &a2; a2.next = &a3; a3.next = &b1;
+	b1.next = &b2; b2.next = &c1;
+	c1.next = &c2;
+	
+	cout << (&b1 == cis14.getClosestCommonWaypoint(&a1, &b1));
+}
+
+/*
+TEST CASES:
+	Node  a1(11), a2(12), a3(13), b1(21), b2(22), c1(31), c2(32), d1(99);
+	a1.next = &a2; a2.next = &a3; a3.next = &c1;
 	b1.next = &b2; b2.next = &c1;
 	c1.next = &c2;
 
-	cout << (&c1 == cis14.getClosestCommonWaypoint(&a, &b1));
-}
+	cout << (&c1 == cis14.getClosestCommonWaypoint(&a1, &c1));	1
+
+	Node  a1(11), a2(12), a3(13), b1(21), b2(22), c1(31), c2(32), d1(99);
+	a1.next = &a2; a2.next = &a3; a3.next = &c1;
+	b1.next = &b2; b2.next = &c1;
+	c1.next = &c2;
+
+	cout << (nullptr == cis14.getClosestCommonWaypoint(nullptr, &c1));	1
+
+	Node  a1(11), a2(12), a3(13), b1(21), b2(22), c1(31), c2(32), d1(99);
+	a1.next = &a2; a2.next = &a3; a3.next = &b1;
+	b1.next = &b2; b2.next = &c1;
+	c1.next = &c2;
+
+	cout << (&b1 == cis14.getClosestCommonWaypoint(&a1, &b1));	1
+
+
+*/
