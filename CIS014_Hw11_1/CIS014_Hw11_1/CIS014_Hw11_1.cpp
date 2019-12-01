74d383cebeb45f8e1c9ee41f665721a4c04e3333
AndrewLam97 <alam1997@gmail.com>
2019-12-01 03:12:42 -0800

    Update CIS014_Hw11_1

--- CIS014_Hw11_1/CIS014_Hw11_1/CIS014_Hw11_1.cpp
+++ CIS014_Hw11_1/CIS014_Hw11_1/CIS014_Hw11_1.cpp
@@ -26,13 +26,30 @@ RETURN VALUES: integer of units of water retained by the terrain
 */
 int getTotalWaterAmount(int* arr, int size)
 {
+	//int total = 0;
+	//for (int i = 1; i < size - 1; i++) { //iterate middle point through array 
+
+	//	int left = arr[i]; //find peak on left
+	//	for (int j = 0; j < i; j++)
+	//	{
+	//		left = max(left, arr[j]);
+	//	}
+
+	//	int right = arr[i]; //find peak on right
+	//	for (int j = i + 1; j < size; j++)
+	//	{
+	//		right = max(right, arr[j]);
+	//	}
+	//	total = total + (min(left, right) - arr[i]);
+	//}
+	//return total;
 	int total = 0;
 	for (int i = 1; i < size - 1; i++) { //iterate middle point through array 
 
 		int left = arr[i]; //find peak on left
 		for (int j = 0; j < i; j++)
 		{
-			left = max(left, arr[j]);
+			left = max(arr[i], arr[j]);
 		}
 
 		int right = arr[i]; //find peak on right
